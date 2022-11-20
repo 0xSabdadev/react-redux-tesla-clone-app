497de991655f59d2d3bb20b732640b4a61017c0f
0xSabdadev <jasonalhilal@gmail.com>
2022-11-20 12:20:28 +0700

    offcanvas components finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
 
-const Header = () => (
-    <Container>
-        <a href='#'>
-            <img src='./images/logo.svg' alt='' />
-        </a>
-        <Menu>
-            <a href='#'>Model S</a>
-            <a href='#'>Model 3</a>
-            <a href='#'>Model X</a>
-            <a href='#'>Model Y</a>
-        </Menu>
-        <RightMenu>
-            <a href='#'>Shop</a>
-            <a href='#'>Tesla Account</a>
-            <CustomMenu />
-        </RightMenu>
-    </Container>
-)
+const Header = () => {
+    const [burgerStatus, setBurgerStatus] = useState(false)
+
+    return (
+        <Container>
+            <a href='#'>
+                <img src='./images/logo.svg' alt='' />
+            </a>
+            <Menu>
+                <a href='#'>Model S</a>
+                <a href='#'>Model 3</a>
+                <a href='#'>Model X</a>
+                <a href='#'>Model Y</a>
+            </Menu>
+            <RightMenu>
+                <a href='#'>Shop</a>
+                <a href='#'>Tesla Account</a>
+                <CustomMenu onClick={() => setBurgerStatus(true)} />
+            </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(false)} />
+                </CloseWrapper>
+                <li>
+                    <a href='#'>Existing Inventory</a>
+                </li>
+                <li>
+                    <a href='#'>Used Inventory</a>
+                </li>
+                <li>
+                    <a href='#'>Trade-in</a>
+                </li>
+                <li>
+                    <a href='#'>Cybertruck</a>
+                </li>
+                <li>
+                    <a href='#'>Roadaster</a>
+                </li>
+                <li>
+                    <a href='#'>Shop Inventory</a>
+                </li>
+                <li>
+                    <a href='#'>Shop Inventory</a>
+                </li>
+                <li>
+                    <a href='#'>Shop Inventory</a>
+                </li>
+            </BurgerNav>
+        </Container>
+    )
+}
 
 export default Header
 
@@ -33,6 +67,7 @@ const Container = styled.div`
     top: 0;
     right: 0;
     left: 0;
+    z-index: 1;
 `
 const Menu = styled.div`
     display: flex;
@@ -60,4 +95,37 @@ const RightMenu = styled.div`
 `
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transition: transform 0.2s;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+        a {
+            font-weight: 600;
+        }
+    }
+    transform: ${props => (props.show ? 'transleX(0)' : 'translateX(100%)')};
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
